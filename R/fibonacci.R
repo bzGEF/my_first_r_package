09e4f11f67816309967deacc2282180534114725
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2018-10-14 18:50:16 +0200

    Added documentation to fib function

--- R/fibonacci.R
+++ R/fibonacci.R
@@ -1,3 +1,10 @@
+#' The n-th Fibonacci number
+#'
+#'Returns the n-th Fibonacci number.
+#'@param n The index of the Fibonacci number to retun
+#'@export
+#'@examples
+#'fib(4)
 fib <- function(n) {
   if (n == 1 | n == 2) {
     1
